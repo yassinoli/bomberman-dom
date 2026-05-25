16e68fc146f7e4720ac475e89dc6563d4d6cd040
yassinoli <yassin890890.890@gmail.com>
2026-05-25 14:04:11 +0100

    start backend

--- backend/main.js
+++ backend/main.js
@@ -1,6 +1,21 @@
 // import html package
 import { createServer } from "http";
 import { Server } from "socket.io";
+import { WebSocket } from "http";
 
 // create http server
 const httpServer = createServer();
+const PORT = 3000;
+
+//upgrade from http to websocket
+const io = new Server(httpServer, {
+  cors: {
+    origin: "*",
+    methods: ["GET", "POST"],
+  },
+});
+
+
+httpServer.listen(PORT, () => {
+  console.log(`Server running at http://localhost:${PORT}/`);
+});
